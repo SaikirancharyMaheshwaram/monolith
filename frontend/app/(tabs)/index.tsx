d4ca4539e31b30e26d6cfa7d325e66d2c2e6566b
saikiranchary <saikiranchary136@gmail.com>
2026-03-03 23:27:06 +0530

    expo and convex init

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,44 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function HomeScreen() {
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <SafeAreaView style={{ paddingHorizontal: 10 }}>
+      <ThemedText>Hello</ThemedText>
+
+      <ThemedView>
+        {tasks?.map(({ _id, text }) => (
+          <ThemedText key={_id}>{text}</ThemedText>
+        ))}
+      </ThemedView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
