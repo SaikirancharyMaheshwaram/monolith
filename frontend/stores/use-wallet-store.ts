8b580cd245e21d02f850774ce2702e92c09bb039
saikiranchary <saikiranchary136@gmail.com>
2026-03-04 03:20:45 +0530

    added persist to zustand

--- frontend/stores/use-wallet-store.ts
+++ frontend/stores/use-wallet-store.ts
@@ -1,11 +1,21 @@
+import { asyncStorageAdapter } from "@/lib/storage";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface WalletState {
   isDevnet: boolean;
   toggleNetwork: () => void;
 }
 
-export const useWalletStore = create<WalletState>((set, get) => ({
-  isDevnet: true,
-  toggleNetwork: () => set((state) => ({ isDevnet: !state.isDevnet })),
-}));
+export const useWalletStore = create<WalletState>()(
+  persist(
+    (set, get) => ({
+      isDevnet: true,
+      toggleNetwork: () => set((state) => ({ isDevnet: !state.isDevnet })),
+    }),
+    {
+      name: "wallet-storage",
+      storage: createJSONStorage(() => asyncStorageAdapter),
+    },
+  ),
+);
